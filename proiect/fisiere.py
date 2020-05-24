84483c15f720433337e5b244956b28111d2e4309
Tavy7 <48602187+Tavy7@users.noreply.github.com>
2020-05-24 19:46:11 +0300

    Update fisiere.py

--- proiect/fisiere.py
+++ proiect/fisiere.py
@@ -1,128 +1,69 @@
-from python_speech_features import mfcc, ssc
-from scipy.io import wavfile
 import numpy as np
 import librosa
 import csv
 import os
 
-import scipy as sp
+def extractFeatures(data, frecv):
+	data = librosa.feature.mfcc(data, sr=frecv)#extragem mfcc din data
+	data = np.mean(data.T, axis=0)#pentru fiecare feature calculam media
 
-extragere = 2
+	return data
+
 
 def getTrainData():
-	print("Citim train")
+	path = "./data/"#initializam path
+	file = csv.reader(open(path + "train.txt"))#deschidem fisierul ce contine labels
+	path += "train/train/"#completam path
 
-	path = "./data/"
-	file = csv.reader(open(path + "train.txt"))
-	path += "train/train/"
-
-	c = 0
-
-	trainData, trainLabels = [], []
+	trainData, trainLabels = [], []#initializam data de returnat
 	for line in file:
 		trainLabels.append(int(line[1]))
 
-		#print("Citim train " + line[0] + " " + str(line[1]))
-		frecv, data = wavfile.read(path + line[0])
-
-		if extragere == 0:
-			data = mfcc(data, frecv)
-		if extragere == 1:
-			data = sp.signal.stft(data, frecv)
-		if extragere == 2:
-			data = ssc(data, frecv)
-
-		trainData.append(data[0])
-
-		#c += 1
-
-		if c == 500:
-			break
-
+		data, frecv = librosa.load(path + line[0])#citim fisierul line[0]
+		data = extractFeatures(data, frecv)#extragem features
+		
+		trainData.append(data)
 	return trainData, trainLabels
 
 
 def getValidationData():
-	print("Citim validation")
-
-
-	path = "./data/"
+	path = "./data/"#initializam path
 	file = csv.reader(open(path + "validation.txt"))
 	path += "validation/validation/"
 
-	c = 0
-
-	validationData, validationLabels = [], []
+	validationData, validationLabels = [], []#initializam data de returnat
 	for line in file:
 		validationLabels.append(int(line[1]))
 
-		#print("Citim validation " + line[0] + " " + str(line[1]))
-		frecv, data = wavfile.read(path + line[0])
+		data, frecv = librosa.load(path + line[0])#citim fisierul line[0]
+		data = extractFeatures(data, frecv)#extragem features
 
-		if extragere == 0:
-			data = mfcc(data, frecv)
-		if extragere == 1:
-			data = sp.signal.stft(data, frecv)
-		if extragere == 2:
-			data = ssc(data, frecv)
-
-		validationData.append(data[0])
-
-		#c += 1
-		if c == 800:
-			break
-
+		validationData.append(data)
 	return validationData, validationLabels
 
-def getTestData():	
-	print("Citim test")
 
-	path = "./data/test/test/"
+def getTestData():	
+	path = "./data/test/test/"#initializam path
 	nume = os.listdir(path)
 
-	testData = []
+	testData = []#initializam data de returnat
 	for i in nume:
-		#print("Citim test " + str(i))
-		frecv, data = wavfile.read(path + i)
-		
-		if extragere == 0:
-			data = mfcc(data, frecv)
-		if extragere == 1:
-			data = sp.signal.stft(data, frecv)
-		if extragere == 2:
-			data = ssc(data, frecv)
+		data, frecv = librosa.load(path + i)#citim fisierul i
+		data = extractFeatures(data, frecv)#extragem features
 
-		testData.append(data[0])
+		testData.append(data)
 	
 	return testData
 
+
 def scrieConcluzie(labels):
 	f = open("output.txt", "a")
-	f.truncate(0)
+	f.truncate(0)#stergere continut fisier
 
 	path = "./data/test/test/"
 	nume = os.listdir(path)
 
-	f.write("name,label\n")
+	f.write("name,label\n")#scriere header
 	for i in range(len(nume)):
-		f.write("{},{}\n".format(nume[i], labels[i]))
+		f.write("{},{}\n".format(nume[i], labels[i]))#scriere nume si label aferent
 	f.close()
-
-
-
-
-
-#59.9, max lbfgs
-#nn =  MLPClassifier(solver = solver, alpha=1e-5, max_iter = 1000)
-
-#60.3, std, rbf
-#60.3 std, sigmoid 
-#svm.SVC(kernel = 'rbf', C = 1.05)
-
-
-def normal(data, norma):
-	if norma == 'x':
-		return data
-
-	from sklearn.preprocessing import normalize
-	return normalize(X = data, norm = norma)
